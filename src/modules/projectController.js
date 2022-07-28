344a36dda219c9844d00a2b6a97a72f961bf7134
Charlie Roeder <croederdev@gmail.com>
2022-07-28 17:21:00 -0400

    form with dropdowns, needs datepicker

--- src/modules/projectController.js
+++ src/modules/projectController.js
@@ -1,10 +1,15 @@
-import {project} from './modules/project';
-import {displayController} from './modules/displayController';
+import {project} from '../modules/project';
+
+let projects = [];
 
 const defaultProject = project('Inbox')
+projects.push(defaultProject);
+
+const chores = project('Chores')
+projects.push(chores)
 
 defaultProject.addEntry('soonest, high-priority', 'abc', new Date(2021, 6, 12), 3)
 defaultProject.addEntry("latest, middle-priority", "def", new Date(2025, 6, 12), 2);
 defaultProject.addEntry('middle, low-priority', 'ghi', new Date(2023, 6, 12), 1)
 
-displayController.buildPage(defaultProject.entries)
+export {projects}
